1d5cfaaacb2c0700d69ea8bf00cd44de02712b05
IodiceDomenico <iodicedomenicoo@libero.it>
2022-10-04 18:42:35 +0200

    Start application

---
+++ prof-advisor-2/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='title'>Prof Advisor</h1>
+      <h2 className='title'>Judge your Prof</h2>
+    <div>
+      <ul>
+        <li className='prof-list'>Prof Alessandro Merola</li>
+      </ul>
+    </div>
+    </div>
+  );
+}
+
+export default App;
